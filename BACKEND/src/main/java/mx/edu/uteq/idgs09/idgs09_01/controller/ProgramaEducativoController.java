ea6ff16c1161425fe880df3d76b6d2825ed50c71
Dev Jack <poncealucard@gmail.com>
2025-06-03 15:18:45 -0600

    Agregue Entidad de programa educativo y el controlador

---
+++ BACKEND/src/main/java/mx/edu/uteq/idgs09/idgs09_01/controller/ProgramaEducativoController.java
@@ -0,0 +1,31 @@
+package mx.edu.uteq.idgs09.idgs09_01.controller;
+
+import org.apache.catalina.connector.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
+
+import mx.edu.uteq.idgs09.idgs09_01.model.entity.Division;
+import mx.edu.uteq.idgs09.idgs09_01.model.entity.ProgramaEducativo;
+import mx.edu.uteq.idgs09.idgs09_01.model.repository.DivisionRepo;
+import mx.edu.uteq.idgs09.idgs09_01.model.repository.ProgramaEducativoRepo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/pe")
+public class ProgramaEducativoController {
+    @Autowired
+    private ProgramaEducativoRepo repo;
+    @Autowired
+    private DivisionRepo drepo;
+
+}
---
+++ BACKEND/src/main/java/mx/edu/uteq/idgs09/idgs09_01/model/repository/ProgramaEducativoRepo.java
@@ -0,0 +1,7 @@
+package mx.edu.uteq.idgs09.idgs09_01.model.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import mx.edu.uteq.idgs09.idgs09_01.model.entity.ProgramaEducativo;
+
+
+public interface ProgramaEducativoRepo extends JpaRepository<ProgramaEducativo,Integer>{
+}
