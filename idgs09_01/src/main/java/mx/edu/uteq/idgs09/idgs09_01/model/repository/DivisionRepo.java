e607c61f9c47dd1bd10fdfc26860534b97020459
Dev Jack <poncealucard@gmail.com>
2025-06-03 11:39:35 -0600

    blank backend

--- idgs09_01/src/main/java/mx/edu/uteq/idgs09/idgs09_01/model/repository/DivisionRepo.java
+++
@@ -1,8 +0,0 @@
-package mx.edu.uteq.idgs09.idgs09_01.model.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import mx.edu.uteq.idgs09.idgs09_01.model.entity.Division;
-
-public interface DivisionRepo extends JpaRepository<Division,Integer>{
-
-    
-}
